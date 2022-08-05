b4c657722b421d828575b3353e0ba27449da187f
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-08-05 20:19:17 -0300

    Implementação do use case de lista de personagens

---
+++ app/src/main/java/com/example/appharrypotter/domain/useCase/CharacterListUseCaseImpl.kt
@@ -0,0 +1,11 @@
+package com.example.appharrypotter.domain.useCase
+
+import com.example.appharrypotter.data.model.Character
+import com.example.appharrypotter.data.repository.CharacterRepository
+
+class CharacterListUseCaseImpl(private val repository: CharacterRepository) : CharacterListUseCase {
+
+    override suspend fun invoke(): List<Character> {
+        return repository.getCharacterList()
+    }
+}
